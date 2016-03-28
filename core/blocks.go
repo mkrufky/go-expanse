d9ea815862a3cae92cde81a3ca6b32289e924f72
Christopher Franko <chrisjfranko@gmail.com>
2016-03-28 08:26:35 -0400

    Invalidate bad block + new nodes
    Added new supernodes
    Invalidate badhash

--- core/blocks.go
+++ core/blocks.go
@@ -21,4 +21,5 @@ import "github.com/expanse-project/go-expanse/common"
 // Set of manually tracked bad hashes (usually hard forks)
 var BadHashes = map[common.Hash]bool{
 	common.HexToHash("05bef30ef572270f654746da22639a7a0c97dd97a7050b9e252391996aaeb689"): true,
+	common.HexToHash("f26dcf2653956030cd913a5ac1572751c97b09e95518c0806751df167a7f965f"): true,
 }
